4218a2fa678cc9805df9edb92ff7a57d12dcb480
I99232 <I99232@IPVAPW7D-B016.iso.com>
2019-01-03 23:08:44 -0500

    pushChanges

---
+++ Test Suites/smokesuite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>smokesuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c38e2608-c5c6-4f6b-bcb2-1d004badf2cd</testSuiteGuid>
+   <testCaseLink>
+      <guid>6e03bcc4-bf5d-491b-b8d4-57531e55f041</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ValidLogin</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
